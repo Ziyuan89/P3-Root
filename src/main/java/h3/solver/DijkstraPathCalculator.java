91e8baf1896bc2c67537573b08a535fff22886c0
Alexander Städing <alexanderstaeding@gmail.com>
2023-06-09 01:21:31 +0200

    Add PathCalculator

---
+++ src/main/java/h3/solver/DijkstraPathCalculator.java
@@ -0,0 +1,11 @@
+package h3.solver;
+
+import java.util.List;
+
+public class DijkstraPathCalculator<N> implements PathCalculator<N> {
+
+    @Override
+    public List<N> calculatePath(final N start, final N end) {
+        return null;
+    }
+}
