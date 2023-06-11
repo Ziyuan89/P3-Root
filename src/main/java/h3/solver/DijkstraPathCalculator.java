1f787626bd4e87d95542f616c09bba3149f5f49b
Alexander Städing <alexanderstaeding@gmail.com>
2023-06-11 23:45:32 +0200

    Add graph to DijkstraPathCalculator

--- src/main/java/h3/solver/DijkstraPathCalculator.java
+++ src/main/java/h3/solver/DijkstraPathCalculator.java
@@ -1,8 +1,16 @@
 package h3.solver;
 
+import h3.graph.Graph;
+
 import java.util.List;
 
 public class DijkstraPathCalculator<N> implements PathCalculator<N> {
+
+    private final Graph<N> graph;
+
+    public DijkstraPathCalculator(Graph<N> graph) {
+        this.graph = graph;
+    }
 
     @Override
     public List<N> calculatePath(final N start, final N end) {
