bd661d194e5711d17855abd127d3f86fd76bca78
LukasKlenner <klenner-lukas@t-online.de>
2023-06-21 22:09:50 +0200

    Only consider connected nodes in Edge.equals and hashCode

--- src/main/java/p3/graph/EdgeImpl.java
+++ src/main/java/p3/graph/EdgeImpl.java
@@ -9,13 +9,12 @@ record EdgeImpl<N>(N a, N b, int weight) implements Edge<N> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EdgeImpl<?> edge = (EdgeImpl<?>) o;
-        return weight == edge.weight &&
-            ((Objects.equals(a, edge.a) && Objects.equals(b, edge.b)) ||
+        return ((Objects.equals(a, edge.a) && Objects.equals(b, edge.b)) ||
                 (Objects.equals(a, edge.b) && Objects.equals(b, edge.a)));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(a, b, weight) + Objects.hash(a, b, weight);
+        return Objects.hash(a, b) + Objects.hash(a, b);
     }
 }
